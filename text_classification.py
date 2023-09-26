30bd74f33943b309bdc97c2ce9594e5303678666
TrustPaul <68587494+TrustPaul@users.noreply.github.com>
2023-09-26 17:07:39 +0100

    Create text_classification.py

---
+++ text_classification.py
@@ -0,0 +1,77 @@
+
+
+from datasets import load_dataset
+import pandas as pd
+imdb = load_dataset("imdb")
+
+texts = imdb['test']['text']
+labels = imdb['test']['label']
+
+df = pd.DataFrame()
+df['text'] = texts
+df['label'] = labels
+df.to_csv('/home/ptrust/experiments/data/test.csv')
+
+data_files = {"train": "/home/ptrust/experiments/data/imdb_facility_2500_cluster_10.csv", "test": "/home/ptrust/experiments/data/test.csv"}
+dataset = load_dataset("csv", data_files=data_files)
+
+imdb = dataset
+from transformers import AutoTokenizer
+model_path = "distilbert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+
+def preprocess_function(examples):
+    return tokenizer(examples["text"], truncation=True)
+
+tokenized_imdb = imdb.map(preprocess_function, batched=True)
+
+from transformers import DataCollatorWithPadding
+
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+import evaluate
+
+accuracy = evaluate.load("accuracy")
+
+import numpy as np
+
+
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    return accuracy.compute(predictions=predictions, references=labels)
+
+id2label = {0: "NEGATIVE", 1: "POSITIVE"}
+label2id = {"NEGATIVE": 0, "POSITIVE": 1}
+
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+
+model = AutoModelForSequenceClassification.from_pretrained(
+    model_path, num_labels=2, id2label=id2label, label2id=label2id
+)
+
+training_args = TrainingArguments(
+    output_dir="my_awesome_model",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=5,
+    weight_decay=0.01,
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    push_to_hub=False,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_imdb["train"],
+    eval_dataset=tokenized_imdb["test"],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
+
